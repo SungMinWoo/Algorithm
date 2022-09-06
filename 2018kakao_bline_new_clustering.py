2a77916fd0a3224c11502bd1cd95148e55476a9e
성민우 <okekdma2@gmail.com>
2022-09-06 21:08:51 +0900

    2018 kakao bline news clustering answer

--- 2018kakao_bline_new_clustering.py
+++ 2018kakao_bline_new_clustering.py
@@ -1,27 +1,25 @@
-import re
-
 def solution(str1, str2):
-    answer = 0
     str1_list = []
     str2_list = []
+    count = 0
     str1, str2 = str1.lower(), str2.lower()
-    if max(str1) >= max(str2):
-        max_len = len(str1)
-    else:
-        max_len = len(str2)
-    for a in range(1, max_len+1):
-        re_str1 = re.sub('[^a-z]', '', str1[a-1:a+1])
-        re_str2 = re.sub('[^a-z]', '', str2[a-1:a+1])
-        if re_str1 == re_str2 and len(re_str2) == 2:
-            answer += 1
-        if len(re_str1) == 2:
-            print(str1[a - 1:a + 1])
-            str1_list.append(str1[a - 1:a + 1])
-        if len(re_str2) == 2:
-            str2_list.append(str2[a - 1:a + 1])
+    for b in range(len(str1) - 1):
+        if str1[b:b + 2].isalpha():
+            str1_list.append(str1[b:b + 2])
+    for b in range(len(str2) - 1):
+        if str2[b:b + 2].isalpha():
+            str2_list.append(str2[b:b + 2])
 
-    # return int((len(set(str2_list)&set(str2_list)) / len(set(str2_list)|set(str2_list))) * 65536)
-    if answer == 0 or len(set(str1_list + str2_list)) == 0:
+    for b in str1_list:
+        if b in str2_list:
+            count += 1
+            str2_list.remove(b)
+
+    all_list = len(str1_list + str2_list) - count
+
+    if count == 0 and all_list != 0:
+        return 0
+    elif count == 0 or all_list == 0:
         return 65536
     else:
-        return int((answer / len(set(str1_list + str2_list))) * 65536)
+        return int(count / all_list * 65536)
